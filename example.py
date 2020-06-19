89fd8b673926a6e282b583e7a4b089ca2a9bc906
wedwrd <66477271+wedwrd@users.noreply.github.com>
2020-06-19 10:23:08 +0100

    Update example.py

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from http.server import HTTPServer
 
 def intent_handler(): # set up what to do with the intents recieved
     if aython.alexa.intent_name == "Hello":
-        auth_code= aython.alexa.auth_code() # if an auth code is sent get it
+        auth_code= aython.alexa.auth_code # if an auth code is sent get it
         aython.alexa.reply_message = alexa.build_speech_response("World") # replys with World
 
 def main():
